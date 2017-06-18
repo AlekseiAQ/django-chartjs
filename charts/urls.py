712a0b5db52c86bb3f48faca063e4df07a1df86b
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-06-17 23:36:24 -0400

    Implemented base charts

--- charts/urls.py
+++ charts/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from .views import ChartData, HomeView, get_data
+
 urlpatterns = [
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^api/data/$', get_data, name='api-data'),
+    url(r'^api/chart/data/$', ChartData.as_view()),
     url(r'^admin/', admin.site.urls),
 ]
